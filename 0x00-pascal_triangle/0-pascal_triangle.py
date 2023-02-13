4f63623f1c5cd3b53bfec02f28e345a0302a886f
Same Titan <49342907+loopopski@users.noreply.github.com>
2023-02-13 05:01:05 -0800

    Pascal's Triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,14 +1,23 @@
 #!/usr/bin/python3
 """
-0-pascal_triangle
+Pascal's Triangle
 """
-pascal_triangle = __import__('0-pascal_triangle').pascal_triangle
-def print_triangle(triangle):
-	"""
-	print the triangle
-	"""
-	for row in triangle:
-		print("[{}]".format(",".join([str(n) for n in row])))
 
-if __name__ = "__pascal__":
-	print_triangle(pascal_triangle(5))
+
+def pascal_triangle(n):
+    """
+         returns a list of lists of
+         integers representing
+          the Pascal’s triangle of n
+         Returns an empty list if n <= 0
+    """
+    if n <= 0:
+        return []
+    triangle = [[1]]
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+        row.append(1)
+        triangle.append(row)
+    return triangle
